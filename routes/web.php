606b037c7573e0cd56d5f951ff935ca30e3c4d00
Tawdi <ahmedtawdi00@gmail.com>
2025-02-20 22:38:11 +0100

    ajoute section de book on home page

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages.home', [
-        'books' => [
-             ['titre' => 'lala'],
-             ['titre' => 'hoho'], 
-             ['titre' => 'momo']
-         ]
-    ]);
-});
+Route::get('/',[HomeController::class,'index']);
 
 Route::get('/login',[AuthController::class,'showLogin']);
 Route::post('/login',[AuthController::class,'login']);
@@ -20,3 +14,8 @@ Route::get('/register',[AuthController::class,'showRegister']);
 Route::post('/register',[AuthController::class,'register']);
 
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+// book
+Route::get('/books', [HomeController::class, 'books'])->name('books');
+Route::get('/book/{book}', [BookController::class, 'show'])->name('book.show');
