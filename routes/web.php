faf73be6cd122d42f551d31a0b13f1bcaf90e960
Tawdi <ahmedtawdi00@gmail.com>
2025-02-21 00:58:43 +0100

    ajoute de routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\HomeController;
@@ -19,3 +20,13 @@ Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 // book
 Route::get('/books', [HomeController::class, 'books'])->name('books');
 Route::get('/book/{book}', [BookController::class, 'show'])->name('book.show');
+Route::post('/book/{id}/book', [BookController::class, 'book'])->name('book.book');
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.index');
+    Route::get('/create', [AdminController::class, 'create'])->name('admin.create');
+    Route::post('/store', [AdminController::class, 'store'])->name('admin.store');
+    Route::get('/edit/{id}', [AdminController::class, 'edit'])->name('admin.edit');
+    Route::put('/update/{id}', [AdminController::class, 'update'])->name('admin.update');
+    Route::delete('/destroy/{id}', [AdminController::class, 'destroy'])->name('admin.destroy');
+});
