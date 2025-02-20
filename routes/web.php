2d40b1cb28849310d24675b85fa866b222509dba
Tawdi <ahmedtawdi00@gmail.com>
2025-02-20 15:26:15 +0100

    les fichiers de laravel

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('pages.home', [
+        'books' => [
+             ['titre' => 'lala'],
+             ['titre' => 'hoho'], 
+             ['titre' => 'momo']
+         ]
+    ]);
+});
+
+Route::get('/login',[AuthController::class,'showLogin']);
+Route::post('/login',[AuthController::class,'login']);
+
+Route::get('/register',[AuthController::class,'showRegister']);
+Route::post('/register',[AuthController::class,'register']);
